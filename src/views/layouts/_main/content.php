c38564509efc9861cdc01bde2b711b436c4c6c50
ZhangYanJiong <zhangyanjiong@163.com>
2019-03-01 18:37:05 +0800

    update layouts

--- src/views/layouts/_main/content.php
+++ src/views/layouts/_main/content.php
@@ -1,14 +1,45 @@
+<?php
+/**
+ * @link https://github.com/devzyj/yii2-application
+ * @copyright Copyright (c) 2019 Zhang Yan Jiong
+ * @license http://opensource.org/licenses/BSD-3-Clause
+ */
+use yii\helpers\Html;
+use yii\helpers\Inflector;
+use yii\widgets\Breadcrumbs;
+use devzyj\yii2\adminlte\widgets\Alert;
+
+/* @var $this \yii\web\View */
+/* @var $content string */
+?>
 <div class="content-wrapper">
     <!-- Content Header (Page header) -->
     <section class="content-header">
-        <h1>Page Header<small>Optional description</small></h1>
-        <ol class="breadcrumb">
-            <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
-            <li class="active">Here</li>
-        </ol>
+        <!-- Content Title -->
+        <?php if (isset($this->blocks['content-header'])): ?>
+            <h1><?= $this->blocks['content-header'] ?></h1>
+        <?php else: ?>
+            <h1>
+                <?php
+                    if ($this->title !== null) {
+                        echo Html::encode($this->title);
+                    } else {
+                        echo Inflector::camel2words(Inflector::id2camel($this->context->module->id));
+                        echo ($this->context->module->id !== \Yii::$app->id) ? '<small>Module</small>' : '';
+                    }
+                ?>
+            </h1>
+        <?php endif; ?>
+        
+        <!-- Breadcrumb -->
+        <?= Breadcrumbs::widget(['links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : []]) ?>
     </section>
     <!-- Main content -->
     <section class="content container-fluid">
+        <!-- Alert box -->
+        <?= Alert::widget()?>
+        
+        <!-- Content box -->
         <?= $content ?>
     </section>
     <!-- /.content -->
--- src/views/layouts/_main/footer.php
+++ src/views/layouts/_main/footer.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
     <!-- To the right -->
-    <div class="pull-right hidden-xs">Anything you want</div>
+    <div class="pull-right hidden-xs"><b>Version</b> 2.4.9</div>
     <!-- Default to the left -->
     <strong>Copyright &copy; 2016 <a href="#">Company</a>.</strong> All rights reserved.
 </footer>
